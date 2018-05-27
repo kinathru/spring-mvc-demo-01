c07edbe057b007e295b450fbdcff623eb3e76907
kinath <kinath.ru@gmail.com>
2018-05-27 15:24:08 +0530

    mvc forms implementation - Request Mapping hierarchically

---
+++ src/main/java/com/kinath/udemy/SillyController.java
@@ -0,0 +1,20 @@
+package com.kinath.udemy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/silly")
+public class SillyController
+{
+    /**
+     * Initial method to show the form.
+     *
+     * @return
+     */
+    @RequestMapping("/showForm")
+    public String displayForm()
+    {
+        return "helloworld-form";
+    }
+}
